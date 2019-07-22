95535beae42a9fcf56a48ae997785eb8f3eae2e0
nchlsschndr <nic.schneider@ymail.com>
2019-07-22 20:45:02 +0200

    rearange functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
-// TODO: optimize setOperator/setoperatorUI … maybe put together 
+// TODO: optimize setOperator
+// TODO: Round numbers
+// TODO: Disable number -> comma -> number -> COMMA
 
 // TODO: keyboard functionality
-// TDOD: handle weird JS behaviour e.g 2.3 - 2.5 = -0.20000000000000018
+// TODO: handle weird JS behaviour e.g 2.3 - 2.5 = -0.20000000000000018
+//       probably a library like decimals.js or big.js
 
 const display = document.querySelector("#display");
 const keys = document.querySelector(".pad");
@@ -9,65 +12,25 @@ const keys = document.querySelector(".pad");
 let currentNum = [];
 let tempNum = null;
 let activeOperator = "";
-let result = undefined;
+let result = null;
 
-function updateDisplay(value) {
-  display.textContent = value;
-}
-
-// converts array (currenNum) to float to calculate with
-function arrToFloat(array) {
-  return parseFloat(array.join(""));
-}
-
-function setOperator(operator) {
-  // only switch numbers if no operator set
-  // with this you can change operator after currentNum is set
-  if (!activeOperator) {
-    tempNum = arrToFloat(currentNum);
-    currentNum = [];
+keys.addEventListener("click", function (e) {
+  // event delegation
+  if (e.target.matches("button")) {
+    handleClick(e);
   }
-  setOperatorUI(operator);
-  activeOperator = operator;
-}
-
-function setOperatorUI(operator) {
-  const buttons = document.querySelectorAll(`button[data-key-type="operator"]`);
-  buttons.forEach(function (el) {
-    el.classList.remove("active");
-  });
-  if (operator === 'reset') {
-    return;
-  }
-  const button = document.querySelector(`button[data-key="${operator}"]`);
-  button.classList.add("active");
-}
-
-function calculate(n1, operator, n2) {
-  switch (operator) {
-    case "plus":
-      return n1 + n2;
-    case "minus":
-      return n1 - n2;
-    case "divide":
-      return n1 / n2;
-    case "multiply":
-      return n1 * n2;
-    default:
-      return "operator error";
-  }
-}
+});
 
 function handleClick(e) {
   const key = e.target.dataset.key;
 
   // check if key is a number
   if (/[0-9]/.test(key)) {
-    // empty everything after 'equals' was clicked to start over
+    // empty everything after there was a previous operation to start over
     if (result) {
       currentNum = [];
+      result = null;
       updateDisplay("0");
-      result = 0;
     }
     currentNum.push(parseInt(key));
     updateDisplay(arrToFloat(currentNum));
@@ -84,11 +47,10 @@ function handleClick(e) {
   }
 
   if (key === "allClear") {
-    setOperatorUI('reset');
     currentNum = [];
     tempNum = null;
-    activeOperator = "";
-    result = undefined;
+    result = null;
+    setOperator('reset');
     updateDisplay("0");
   }
 
@@ -104,7 +66,7 @@ function handleClick(e) {
     key === "divide" ||
     key === "multiply"
   ) {
-    // only if a number was clicked or tempNum is set
+    // only if a number was clicked or tempNum is not empty
     if (currentNum.length > 0 || tempNum != null) {
       equals();
       setOperator(key);
@@ -114,6 +76,7 @@ function handleClick(e) {
   if (key === "equals") {
     equals();
   }
+
   // debug
   document.querySelector('.temp-num').textContent = `-------tempNum: ${tempNum}`;
   document.querySelector('.active-operator').textContent = `activeOperator: ${activeOperator}`;
@@ -121,20 +84,61 @@ function handleClick(e) {
   document.querySelector('.result').textContent = `--------result: ${result}`;
 }
 
-keys.addEventListener("click", function (e) {
-  // event delegation
-  if (e.target.matches("button")) {
-    handleClick(e);
+
+
+function updateDisplay(value) {
+  display.textContent = value;
+}
+
+// converts array (currenNum) to float to calculate with
+function arrToFloat(array) {
+  return parseFloat(array.join(""));
+}
+
+function setOperator(operator) {
+  const buttons = document.querySelectorAll(`button[data-key-type="operator"]`);
+  buttons.forEach(function (el) {
+    el.classList.remove("active");
+  });
+
+  if (operator === 'reset') {
+    activeOperator = '';
+    return;
   }
-});
+
+  // only switch numbers if no operator set
+  // with this you can change operator after currentNum is set
+  if (!activeOperator) {
+    tempNum = arrToFloat(currentNum);
+    currentNum = [];
+  }
+  activeOperator = operator;
+
+  const button = document.querySelector(`button[data-key="${operator}"]`);
+  button.classList.add("active");
+}
 
 function equals() {
   if (tempNum != null && currentNum.length > 0) {
     result = calculate(tempNum, activeOperator, arrToFloat(currentNum));
-    updateDisplay(result);
-    currentNum = [result]; // to work with
     tempNum = null;
-    setOperatorUI('reset');
-    activeOperator = "";
+    currentNum = [result]; // to calculate with
+    setOperator('reset');
+    updateDisplay(result);
+  }
+}
+
+function calculate(n1, operator, n2) {
+  switch (operator) {
+    case "plus":
+      return n1 + n2;
+    case "minus":
+      return n1 - n2;
+    case "divide":
+      return n1 / n2;
+    case "multiply":
+      return n1 * n2;
+    default:
+      return "operator error";
   }
 }
